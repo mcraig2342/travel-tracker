a8c66ccbc23c083b0d1cbd15a4bf42190c1cf1ad
mcraig2342 <matthewcookecraig10@gmail.com>
2021-04-22 12:18:57 -0600

    Add properties to trip class

--- test/trip-test.js
+++ test/trip-test.js
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
-import { trips } from '../src/data/trip-test-data.js'
-import { destinations } from '../src/data/destination-test-data.js'
+import trips from '../src/data/trip-test-data.js'
+import destinations from '../src/data/destination-test-data.js'
 import Trip from '../src/trip.js';
 
 let trip;
@@ -21,11 +21,11 @@ describe('Trip', () => {
   });
 
   it("should have a user id", () => {
-    expect(trip.userId).to.equal(2);
+    expect(trip.userID).to.equal(2);
   });
 
   it("should have a destination id", () => {
-    expect(trip.destinationId).to.equal(5);
+    expect(trip.destinationID).to.equal(5);
   });
 
   it("should have a number of travelers", () => {
@@ -45,27 +45,12 @@ describe('Trip', () => {
   });
 
   it("should have an array of suggested activities", () => {
-    expect(trip.suggestedActivities).to.equal([]);
+    expect(trip.suggestedActivities).to.deep.equal([]);
   });
 
-  it("should have a destination name", () => {
-    expect(trip.destinationName).to.equal('Madrid, Spain');
+  it("should have a correspondong destination object", () => {
+    expect(trip.correspondingDestination).to.deep.equal(destinationData[4]);
   });
 
-  it("should have a cost per day", () => {
-    expect(trip.costPerDay).to.equal(100);
-  });
-
-  it("should have a flight cost per person", () => {
-    expect(trip.FlightCostPerPerson).to.equal(780);
-  });
-
-  it("should have an image", () => {
-    expect(trip.image).to.equal('https://images.unsplash.com/photo-1560089168-6516081f5bf1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80');
-  });
-
-  it("should have alt text", () => {
-    expect(trip.alt).to.equal('city with boats on the water during the day time');
-  });
 
 });
