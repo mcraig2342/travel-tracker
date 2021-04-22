0b486cbf009b44444c47a8727bf4286cf2c0cbfd
mcraig2342 <matthewcookecraig10@gmail.com>
2021-04-22 15:23:20 -0600

    Get initial data sets

---
+++ src/network-requests.js
@@ -0,0 +1,32 @@
+const fetchData = () => {
+  const tripsData = fetch('http://localhost:3001/api/v1/trips')
+    .then(response => response.json())
+    .then(tripsData => {
+      return tripsData;
+    })
+
+  const destinationData = fetch('http://localhost:3001/api/v1/destinations')
+    .then(response => response.json())
+    .then(destinationData => {
+      return destinationData;
+    })
+
+  const travelerData = fetch('http://localhost:3001/api/v1/travelers')
+    .then(response => response.json())
+    .then(travelerData => {
+      return travelerData;
+    })
+
+  return Promise.all([tripsData, destinationData, userData])
+    .then(data => {
+      const allData = {};
+      allData.tripsData = data[0];
+      allData.destinationData = data[1];
+      allData.travelerData = data[2];
+      return allData;
+    })
+    .catch(err => console.log('ERROR', err))
+}
+
+
+export { fetchData };
